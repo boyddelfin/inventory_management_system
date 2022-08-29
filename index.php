1392168620778ec32dd33fcbed7d0809f868932a
Boyd Delfin <vadjr27@gmail.com>
2022-08-29 20:48:22 +0800

    Cleaning index

--- index.php
+++ index.php
@@ -3,10 +3,10 @@ define('APP_DIR', __DIR__);
 require APP_DIR."/back-end/loader.php";
 include APP_DIR."/front-end/template/header.php";
 // Page Template Start
-if(!isset($_SESSION['id'])) {
-    include APP_DIR.'/front-end/login.php';
-} else {
+if($sessions->is_set()) {
     include APP_DIR.'/front-end/dashboard.php';
+} else {
+    include APP_DIR.'/front-end/login.php';
 }
 // Page Template End
 include APP_DIR."/front-end/template/footer.php";
