8816b9a1c57067c63bb4c6f4c2a2d71f759334c2
Boyd Delfin <vadjr27@gmail.com>
2022-08-29 20:45:32 +0800

    Added login and logout function

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php 
-
 define('APP_DIR', __DIR__);
 require APP_DIR."/back-end/loader.php";
+// --------------------------------------------------------
+
+// --------------------------------------------------------
 include APP_DIR."/front-end/template/header.php";
 // Page Template Start
 if(!isset($_SESSION['id'])) {
-    $sessions->destroy();
     include APP_DIR.'/front-end/login.php';
 } else {
     include APP_DIR.'/front-end/dashboard.php';
